a1e6dee7a6ba71e56dbf2fb6ec2ea56b676ba6e2
J Khalaf <jwan.khalaf@outlook.com>
2019-03-23 22:14:10 +0000

    added the feature/travis-ci-build to the case switch for travis

--- scripts/docker-publish-travis.sh
+++ scripts/docker-publish-travis.sh
@@ -10,16 +10,20 @@ case "$TRAVIS_BRANCH" in
     DOCKER_ENV=development
     DOCKER_TAG=dev
     ;;
+  "feature/travis-ci-build")
+    DOCKER_ENV=development
+    DOCKER_TAG=dev
+    ;;
 esac
 
 # log into docker hub.
 docker login -u $DOCKER_USERNAME -p $DOCKER_PASSWORD
 
 # build the docker image.
-docker build -f ./Bejebeje.Identity/Dockerfile.$DOCKER_ENV -t bejebeje-identity:$DOCKER_TAG ./Bejebeje.Identity --no-cache
+docker build -f ./Bejebeje.Identity/Dockerfile.$DOCKER_ENV -t bejebeje/identity:$DOCKER_TAG ./Bejebeje.Identity --no-cache
 
 # tag the docker image.
-docker tag bejebeje-identity:$DOCKER_TAG $DOCKER_USERNAME/bejebeje-identity:$DOCKER_TAG
+docker tag bejebeje/identity:$DOCKER_TAG $DOCKER_USERNAME/bejebeje/identity:$DOCKER_TAG
 
 # push the docker image to docker hub.
-docker push $DOCKER_USERNAME/bejebeje-identity:$DOCKER_TAG
+docker push $DOCKER_USERNAME/bejebeje/identity:$DOCKER_TAG
