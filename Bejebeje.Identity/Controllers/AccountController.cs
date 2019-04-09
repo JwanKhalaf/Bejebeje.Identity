21ffc67a384cc845e80ec3dd6927e89ada00a0fe
J Khalaf <jwan.khalaf@outlook.com>
2019-04-09 22:27:38 +0100

    added registration methods

--- Bejebeje.Identity/Controllers/AccountController.cs
+++ Bejebeje.Identity/Controllers/AccountController.cs
@@ -47,6 +47,44 @@ namespace Bejebeje.Identity.Controllers
             _events = events;
         }
 
+        [HttpGet]
+        public async Task<IActionResult> Register()
+        {
+            RegisterViewModel registerViewModel = new RegisterViewModel();
+            return View(registerViewModel);
+        }
+
+        public async Task<IActionResult> Register(RegisterViewModel model)
+        {
+          if (ModelState.IsValid)
+          {
+            var user = new BejebejeUser { UserName = model.Email, Email = model.Email };
+            var result = await _userManager.CreateAsync(user, model.Password);
+            if (result.Succeeded)
+            {
+              _logger.LogInformation("User created a new account with password.");
+
+              var code = await _userManager.GenerateEmailConfirmationTokenAsync(user);
+              
+              var callbackUrl = Url.Page(
+                  "/Account/ConfirmEmail",
+                  pageHandler: null,
+                  values: new { userId = user.Id, code = code },
+                  protocol: Request.Scheme);
+
+              await _emailSender.SendEmailAsync(Input.Email, "Confirm your email",
+                  $"Please confirm your account by <a href='{HtmlEncoder.Default.Encode(callbackUrl)}'>clicking here</a>.");
+
+              await _signInManager.SignInAsync(user, isPersistent: false);
+              return LocalRedirect(returnUrl);
+            }
+            foreach (var error in result.Errors)
+            {
+              ModelState.AddModelError(string.Empty, error.Description);
+            }
+          }
+        }
+
         /// <summary>
         /// Entry point into the login workflow
         /// </summary>
