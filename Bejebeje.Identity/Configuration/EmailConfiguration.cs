1610e0fe80cb658a6831305859df60be2fd78db7
J Khalaf <jwan.khalaf@outlook.com>
2019-04-11 21:00:15 +0100

    renamed variables

--- Bejebeje.Identity/Configuration/EmailConfiguration.cs
+++ Bejebeje.Identity/Configuration/EmailConfiguration.cs
@@ -2,7 +2,7 @@ namespace Bejebeje.Identity.Configuration
 {
   public class EmailConfiguration
   {
-    public string ApplicationOutgoingEmailAddress { get; set; }
+    public string OutgoingEmailAddress { get; set; }
     public string SmtpHost { get; set; }
     public int SmtpPort { get; set; }
     public string SmtpServerUsername { get; set; }
