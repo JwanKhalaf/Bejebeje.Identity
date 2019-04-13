8841ba781899380ada38bcaeffa7d90c63d69798
J Khalaf <jwan.khalaf@outlook.com>
2019-04-13 15:11:38 +0100

    configured gulp to run tasks before project build, and also ported the old css to scss

--- Bejebeje.Identity/gulpfile.js
+++ Bejebeje.Identity/gulpfile.js
@@ -1,4 +1,4 @@
-/// <binding Clean='clean' />
+/// <binding BeforeBuild='clean' Clean='clean' />
 "use strict";
 
 const gulp = require("gulp");
@@ -6,6 +6,9 @@ const rimraf = require("rimraf");
 const concat = require("gulp-concat");
 const cssmin = require("gulp-cssmin");
 const uglify = require("gulp-uglify");
+const sass = require("gulp-sass");
+
+sass.compiler = require("node-sass");
 
 const paths = {
   webroot: "./wwwroot/"
@@ -13,10 +16,11 @@ const paths = {
 
 paths.js = paths.webroot + "js/**/*.js";
 paths.minJs = paths.webroot + "js/**/*.min.js";
+paths.sass = paths.webroot + "sass/**/*.scss";
 paths.css = paths.webroot + "css/**/*.css";
 paths.minCss = paths.webroot + "css/**/*.min.css";
 paths.concatJsDest = paths.webroot + "js/site.min.js";
-paths.concatCssDest = paths.webroot + "css/site.min.css";
+paths.concatCssDest = paths.webroot + "css/main.min.css";
 
 gulp.task("clean:js", done => rimraf(paths.concatJsDest, done));
 gulp.task("clean:css", done => rimraf(paths.concatCssDest, done));
@@ -30,6 +34,13 @@ gulp.task("min:js", () => {
     .pipe(gulp.dest("."));
 });
 
+gulp.task("compile:sass", () => {
+  return gulp
+    .src([paths.sass])
+    .pipe(sass().on("error", sass.logError))
+    .pipe(gulp.dest(paths.webroot + "css/"));
+});
+
 gulp.task("min:css", () => {
   return gulp
     .src([paths.css, "!" + paths.minCss])
@@ -41,4 +52,4 @@ gulp.task("min:css", () => {
 gulp.task("min", gulp.series(["min:js", "min:css"]));
 
 // A 'default' task is required by Gulp v4
-gulp.task("default", gulp.series(["min"]));
+gulp.task("default", gulp.series(["clean", "compile:sass", "min"]));
