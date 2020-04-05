67ff1f5acde7b50ab54c7aa8599db7a626b39006
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-04-05 23:40:51 +0100

    add bejebeje admin client

--- Bejebeje.Identity/Configuration/Config.cs
+++ Bejebeje.Identity/Configuration/Config.cs
@@ -1,16 +1,17 @@
-﻿using IdentityServer4.Models;
-using System.Collections.Generic;
-using Microsoft.Extensions.Options;
-
-namespace Bejebeje.Identity.Configuration
+﻿namespace Bejebeje.Identity.Configuration
 {
+  using IdentityServer4.Models;
+  using System.Collections.Generic;
+  using IdentityServer4;
+  using Microsoft.Extensions.Options;
+
   public class Config
   {
-    private InitialIdentityServerConfiguration identityServerConfiguration { get; set; }
+    private InitialIdentityServerConfiguration IdentityServerConfiguration { get; set; }
 
     public Config(IOptions<InitialIdentityServerConfiguration> initialIdentityServerConfiguration)
     {
-      identityServerConfiguration = initialIdentityServerConfiguration.Value;
+      IdentityServerConfiguration = initialIdentityServerConfiguration.Value;
     }
 
     public IEnumerable<IdentityResource> GetIdentityResources()
@@ -26,7 +27,7 @@ namespace Bejebeje.Identity.Configuration
     {
       return new ApiResource[]
       {
-        new ApiResource(identityServerConfiguration.ApiName, "Bejebeje API"),
+        new ApiResource(IdentityServerConfiguration.ApiName, "Bejebeje API"),
         new ApiResource("bejebeje-api-local", "Bejebeje API")
       };
     }
@@ -37,7 +38,7 @@ namespace Bejebeje.Identity.Configuration
       {
         new Client
         {
-          ClientId = identityServerConfiguration.FrontendClientId,
+          ClientId = IdentityServerConfiguration.FrontendClientId,
           ClientName = "Bejebeje ReactJS SPA Client",
           AllowedGrantTypes = GrantTypes.Code,
           RequirePkce = true,
@@ -46,7 +47,7 @@ namespace Bejebeje.Identity.Configuration
           RedirectUris = { "https://bejebeje.com/login-callback" },
           PostLogoutRedirectUris = { "https://bejebeje.com/logout-callback" },
           AllowedCorsOrigins = { "https://bejebeje.com" },
-          AllowedScopes = { "openid", "profile", identityServerConfiguration.ApiName },
+          AllowedScopes = { IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, IdentityServerConfiguration.ApiName },
           AllowOfflineAccess = true,
           RefreshTokenUsage = TokenUsage.ReUse,
         },
@@ -61,10 +62,46 @@ namespace Bejebeje.Identity.Configuration
           RedirectUris = { "http://localhost:1234/login-callback" },
           PostLogoutRedirectUris = { "http://localhost:1234/logout-callback" },
           AllowedCorsOrigins = { "http://localhost:1234" },
-          AllowedScopes = { "openid", "profile", "bejebeje-api-local" },
+          AllowedScopes = { IdentityServerConstants.StandardScopes.OpenId, IdentityServerConstants.StandardScopes.Profile, "bejebeje-api-local" },
           AllowOfflineAccess = true,
           RefreshTokenUsage = TokenUsage.ReUse,
-        }
+        },
+        new Client
+        {
+          ClientId = IdentityServerConfiguration.AdminClientId,
+          ClientName = "bejebeje admin mvc client",
+          ClientSecrets = { new Secret(IdentityServerConfiguration.AdminClientSecret.Sha256()) },
+          AllowedGrantTypes = GrantTypes.Code,
+          RequireConsent = false,
+          RequirePkce = true,
+          // where to redirect to after login
+          RedirectUris = { $"{IdentityServerConfiguration.AdminEndpoint}/signin-oidc" },
+          // where to redirect to after logout
+          PostLogoutRedirectUris = { $"{IdentityServerConfiguration.AdminEndpoint}/signout-callback-oidc" },
+          AllowedScopes = new List<string>
+          {
+            IdentityServerConstants.StandardScopes.OpenId,
+            IdentityServerConstants.StandardScopes.Profile
+          }
+        },
+        new Client
+        {
+          ClientId = "bejebeje-admin-mvc-local",
+          ClientName = "bejebeje admin mvc client",
+          ClientSecrets = { new Secret("secret".Sha256()) },
+          AllowedGrantTypes = GrantTypes.Code,
+          RequireConsent = false,
+          RequirePkce = true,
+          // where to redirect to after login
+          RedirectUris = { "https://localhost:5021/signin-oidc" },
+          // where to redirect to after logout
+          PostLogoutRedirectUris = { "https://localhost:5021/signout-callback-oidc" },
+          AllowedScopes = new List<string>
+          {
+            IdentityServerConstants.StandardScopes.OpenId,
+            IdentityServerConstants.StandardScopes.Profile
+          }
+        },
       };
     }
   }
--- Bejebeje.Identity/Configuration/InitialIdentityServerConfiguration.cs
+++ Bejebeje.Identity/Configuration/InitialIdentityServerConfiguration.cs
@@ -3,10 +3,21 @@ namespace Bejebeje.Identity.Configuration
   public class InitialIdentityServerConfiguration
   {
     public string ApiName { get; set; }
+
     public string FrontendClientId { get; set; }
+
+    public string AdminClientId { get; set; }
+
+    public string AdminClientSecret { get; set; }
+
+    public string AdminEndpoint { get; set; }
+
     public string GoogleClientId { get; set; }
+
     public string GoogleClientSecret { get; set; }
+
     public string FacebookClientId { get; set; }
+
     public string FacebookClientSecret { get; set; }
   }
 }
