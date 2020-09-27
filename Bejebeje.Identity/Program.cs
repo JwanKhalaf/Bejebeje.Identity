b4f67e59a64424c4e7b31e3666dbed091a0dab9e
Jwan Khalaf <jwan.khalaf@outlook.com>
2020-09-27 20:18:39 +0100

    run project in https

--- Bejebeje.Identity/Program.cs
+++ Bejebeje.Identity/Program.cs
@@ -66,9 +66,9 @@
             {
               configuration
                 .MinimumLevel.Debug()
-                //.MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
-                //.MinimumLevel.Override("System", LogEventLevel.Warning)
-                //.MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
+                .MinimumLevel.Override("Microsoft", LogEventLevel.Warning)
+                .MinimumLevel.Override("System", LogEventLevel.Warning)
+                .MinimumLevel.Override("Microsoft.AspNetCore.Authentication", LogEventLevel.Information)
                 .Enrich.FromLogContext()
                 .WriteTo.Console(
                   outputTemplate: "[{Timestamp:HH:mm:ss} {Level}] {SourceContext}{NewLine}{Message:lj}{NewLine}{Exception}{NewLine}",
